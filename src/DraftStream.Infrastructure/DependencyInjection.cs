333cf884301c14d8dc906789993ce07a8c9221ab
MarinCervinschi <marincervinschi3@gmail.com>
2026-02-14 17:56:56 +0100

    Refactor AddWorkflowHandlers method for improved type handling and clarity

--- src/DraftStream.Infrastructure/DependencyInjection.cs
+++ src/DraftStream.Infrastructure/DependencyInjection.cs
@@ -16,15 +16,15 @@ public static class DependencyInjection
 
     private static void AddWorkflowHandlers(this IServiceCollection services)
     {
-        var handlerTypes = AppDomain.CurrentDomain.GetAssemblies()
+        IEnumerable<Type> handlerTypes = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(a => a.GetTypes())
             .Where(t => t is { IsClass: true, IsAbstract: false }
                         && typeof(IWorkflowHandler).IsAssignableFrom(t)
                         && t.GetCustomAttribute<WorkflowHandlerAttribute>() is not null);
 
-        foreach (var type in handlerTypes)
+        foreach (Type type in handlerTypes)
         {
-            var name = type.GetCustomAttribute<WorkflowHandlerAttribute>()!.Name;
+            string name = type.GetCustomAttribute<WorkflowHandlerAttribute>()!.Name;
             services.AddKeyedSingleton(typeof(IWorkflowHandler), name, type);
         }
     }
